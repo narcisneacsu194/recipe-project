5b156446bc94b5de68c8c452000181ee62bd2013
Narcis Neacsu <narcis.neacsu@doc-process.com>
2018-06-09 22:48:43 +0300

    Added the capability to post a recipe using a form and view its details on a web page.

--- src/main/java/com/company/controllers/RecipeController.java
+++ src/main/java/com/company/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.company.controllers;
 
+import com.company.commands.RecipeCommand;
 import com.company.domain.Recipe;
 import com.company.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Set;
@@ -19,7 +22,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
@@ -34,5 +37,20 @@ public class RecipeController {
         model.addAttribute("recipes", recipes);
 
         return "recipes";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand recipeCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + recipeCommand.getId();
     }
 }
