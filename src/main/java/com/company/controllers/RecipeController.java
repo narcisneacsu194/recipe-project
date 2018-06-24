c9268bf6cc84686dcc06038c26f7d723c4bc75fe
Narcis Neacsu <narcis.neacsu@doc-process.com>
2018-06-24 10:28:24 +0300

    Added error page to be used by the written handler when exception is thrown.

--- src/main/java/com/company/controllers/RecipeController.java
+++ src/main/java/com/company/controllers/RecipeController.java
@@ -2,11 +2,14 @@ package com.company.controllers;
 
 import com.company.commands.RecipeCommand;
 import com.company.domain.Recipe;
+import com.company.exceptions.NotFoundException;
 import com.company.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -65,5 +68,16 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
